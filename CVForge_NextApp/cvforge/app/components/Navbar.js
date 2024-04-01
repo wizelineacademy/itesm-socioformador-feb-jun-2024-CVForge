b79de96641930890f8791d11e59b98d872a81934
a01721732 <68079249+a01721732@users.noreply.github.com>
2024-04-01 10:13:27 -0600

    Navbar with buttons Prototype

--- CVForge_NextApp/cvforge/app/components/Navbar.js
+++ CVForge_NextApp/cvforge/app/components/Navbar.js
@@ -3,8 +3,28 @@ import React from 'react'
 
 function Navbar() {
     return (
-        <div className='text-black px-24 py-5 bg-white shadow-md'>
+        <div className='text-black px-24 py-5 bg-white shadow-md flex'>
             <h1 className='font-koh_santepheap text-4xl font-bold'>CVForge<span className='text-green-400'>.ai</span></h1>
+            <div className='text-right px-20 menu'>
+                <nav>
+                    <ul className='flex gap-[30%] py-3'>
+                        <li href=''>
+                            Home
+                        </li>
+                        <li>
+                            Editor
+                        </li>
+                        <li>
+                            Roadmap
+                        </li>
+                        <li>
+                            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                                button
+                            </button>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
         </div>
     )
 }
