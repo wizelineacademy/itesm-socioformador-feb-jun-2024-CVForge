62f1cc9af73b46393f305f8b4269386cea5b25f7
YuvanThirukumaran1 <a00834121@tec.mx>
2024-04-09 11:57:24 -0600

    signin

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,12 +1,12 @@
 "use client";
 import React from "react";
-import Login from "./login/components/Login";
 import '../../style/globals.css'
+import Signin from "./signin/components/Signin";
 
 const auth: React.FC = ({ }) => {
     return (
         <main className="w-full h-full">
-        <Login />
+<Signin/>
       </main>
     )
 }
