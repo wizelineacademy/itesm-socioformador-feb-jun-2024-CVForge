a8797c5ad0a8986cbb42cccb40c85370481fe535
YuvanThirukumaran1 <a00834121@tec.mx>
2024-04-09 01:05:06 -0600

    loginbutton1

--- app/auth/login/components/Login.tsx
+++ app/auth/login/components/Login.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import MainLogo from '@/public/assets/MainLogo'; // Import the MainLogo component
 
-const Login: React.FC = () => {
+const Login = () => {
     // Extracted width and height from the Google button's styles
     const buttonStyle = {
         width: '350px',
         height: '48px',
+        display: 'flex',
+        alignItems: 'center', // Align items vertically in the button
+        justifyContent: 'center', // Align items horizontally in the button
     };
 
     return (
@@ -21,20 +24,22 @@ const Login: React.FC = () => {
                 <div className="text-center">
                     <h2 className="text-2xl font-bold mb-4">Try CVForge.ai now!</h2>
                     {/* Signup buttons */}
-                    <div className="mb-20">
-                        <div className="mb-5">
-                            <button className="bg-gradient-to-r from-emerald-400 to-cyan-400 text-white font-bold rounded-full" style={buttonStyle}>
-                                Sign up with Google
+                    <div className="mb-21">
+                        <div className="mb-6">
+                            <button className="text-gptgreen font-bold border-gradient rounded-full" style={buttonStyle}>
+                                <img src="path_to_google_icon"className="mr-2" /> Sign up with Google
                             </button>
                         </div>
                         <div className="mb-2">
-                            <button className="bg-gradient-to-r from-emerald-400 to-cyan-400 text-white font-bold rounded-full" style={buttonStyle}>
-                                Sign up with LinkedIn
+                            <button className="text-gptgreen font-bold border-gradient rounded-full" style={buttonStyle}>
+                                <img src="path_to_linkedin_icon"className="mr-2" /> Sign up with LinkedIn
                             </button>
                         </div>
                     </div>
+                    {/* Line between Sign up with LinkedIn and Create Account */}
+                    <hr className="my-10 border-gray-400"/>
                     {/* Create account button */}
-                    <div className="mb-10">
+                    <div className="mb-6">
                         <button className="bg-gradient-to-r from-emerald-400 to-cyan-400 text-white font-bold rounded-full mb-4" style={buttonStyle}>
                             Create Account
                         </button>
