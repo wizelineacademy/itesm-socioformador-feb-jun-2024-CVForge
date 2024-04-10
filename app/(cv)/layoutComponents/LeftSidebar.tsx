7b2cd788569000a16944f13da7552a57a955ff82
Marco Podesta <59153650+marcopod@users.noreply.github.com>
2024-04-10 14:56:06 -0600

    Sidebar modified

--- app/(cv)/layoutComponents/LeftSidebar.tsx
+++ app/(cv)/layoutComponents/LeftSidebar.tsx
@@ -9,32 +9,62 @@ import CV_icon from "@/public/assets/cv/CV_icon"
 import PersonalInfo_icon from "@/public/assets/cv/PersonalInfo_icon"
 import { RootState } from "@/contexts/cv/RootState"
 import Link from "next/link";
+import React, { ReactElement, ReactNode } from "react";
+
+type SidebarListElementProps = {
+    title: string;
+    currentTab: string; // Current tab value
+    itemTab: string; // To check if it is the same as the current tab
+    href: string; // Link to redirect
+    icon: ReactElement;
+};
+
+const SidebarListElement: React.FC<SidebarListElementProps> = ({ title, currentTab, href, itemTab, icon }) => {
+    // The color to be assigned
+    const color = currentTab === itemTab ? "white" : "#7E7E7E";
+
+    // The icon for the item
+    const IconComponent = React.cloneElement(icon, { strokeColor: color });
+
+    return (
+        <li className="my-6">
+            <Link href={href} className="flex flex-row items-center">
+                <div className="w-10 h-10">
+                    {IconComponent}
+                </div>
+                <p className={`ml-2 text-[${color}]`}>
+                    {title}
+                </p>
+            </Link>
+        </li>
+    );
+};
 
 const LeftSidebar = () => {
+    // Context that stores the current tab being displayed, to add styling accordingly
     const currentTab = useSelector((state: RootState) => state.currentTab)
+
     return (
         <aside className="fixed top-0 left-0 h-screen w-64 bg-[#474646] text-white flex flex-col p-10">
             <div className="w-full h-10"><SecondaryLogo /></div>
-            <ul>
-                <li>
-                    <Link href={"/cv_gallery"}
-                        className="flex flex-row items-center">
-                        <div className="w-10 h-10">
-                            <CV_icon />
-                        </div>
-                        <p className="ml-2">CVs</p>
-                    </Link>
-                </li>
-                <li className="flex flex-row items-center">
-                    <Link href={"/info"}
-                        className="flex flex-row items-center">
-                        <div className="w-10 h-10">
-                            <PersonalInfo_icon />
-                        </div>
-                        <p className="ml-2">Information Editor</p>
-                    </Link>
-                </li>
-            </ul>
+            <div className="mt-6">
+                <ul>
+                    <SidebarListElement
+                        title="CVs"
+                        icon={<CV_icon />}
+                        currentTab={currentTab}
+                        href="/cv_gallery"
+                        itemTab="cv_gallery"
+                    />
+                    <SidebarListElement
+                        title="Information Editor"
+                        icon={<PersonalInfo_icon />}
+                        currentTab={currentTab}
+                        href="/info"
+                        itemTab="info"
+                    />
+                </ul>
+            </div>
         </aside>
     )
 }
