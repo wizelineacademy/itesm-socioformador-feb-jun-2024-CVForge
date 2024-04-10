351ba98d6a90c3a6d71af9145acfe4b4255b4255
YuvanThirukumaran1 <a00834121@tec.mx>
2024-04-10 10:43:06 -0600

    userpage

---
+++ app/auth/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+import React from "react";
+import '../../style/globals.css'
+import Useroptions from "./Useroptions/components/useroptions";
+
+const auth: React.FC = ({ }) => {
+    return (
+        <main className="w-full h-full">
+            <Useroptions/>
+      </main>
+    )
+}
+
+export default auth;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,21 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        'gptgreen': "#74AA9C",
+        'normaltext': "#474646"
+      },
+      fontFamily: {
+        koh_santepheap: ["Koh-Santepheap"],
+        inter: ["Inter"],
+        
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      
     },
   },
   plugins: [],
