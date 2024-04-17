4e9e1638a7e27cdf2df22f112ab697eb4d6e1c9b
Marco Podesta <59153650+marcopod@users.noreply.github.com>
2024-04-17 09:53:32 -0600

    user service

--- services/user.ts
+++ services/user.ts
@@ -3,9 +3,11 @@ import prisma from '@/lib/prisma';
 
 const baseUrl = process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_API_URL : 'http://localhost:3001';
 
-export async function findUserById(userId: string) {
+const findUserById = async (userId: string) => {
   const user = await prisma.user.findUnique({
     where: { user_id: userId },
   });
   return user;
 }
+
+
