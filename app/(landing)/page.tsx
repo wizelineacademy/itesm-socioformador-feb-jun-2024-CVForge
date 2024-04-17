822ef72b92d5722091a7d9900ce931cd0fab0e9e
a01721732 <68079249+a01721732@users.noreply.github.com>
2024-04-17 17:12:07 -0600

    Google OAuth Configuration

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -4,11 +4,14 @@ import Footer from "./components/Footer";
 import LandingContent from "./call_to_actions/Landing_Content";
 import LandingFeature from "./call_to_actions/Landing_Feature";
 import LandingPosition from "./call_to_actions/Landing_Position";
+import { getUserSession } from "@/lib/session";
 
 
 const Home: React.FC = () => {
+  const user = getUserSession()
   return (
     <div className="flex flex-col min-h-screen bg-slate-100">
+      <main>{JSON.stringify(user)}</main>
       <Navbar />
       <LandingContent/>
       <LandingFeature/>
