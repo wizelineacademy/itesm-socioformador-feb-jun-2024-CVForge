2b6527e595a170ce423a12023145ed42fb013545
gaelgarcia003 <105666768+AngelGaelGarciaRangel@users.noreply.github.com>
2024-04-17 10:16:59 -0600

    modifiedÃ: user service

--- services/user.ts
+++ services/user.ts
@@ -1,13 +1,11 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import prisma from '@/lib/prisma';
+/*import prisma from '@/lib/prisma';
 
-const findUserById = async (userId: string) => {
+// Validar JWT (Jason Web Token)
+// Nomenclatura: Usar Find
+//CREATE, GET ALL, GET
+export async function findUserByI(userId: string) {
   const user = await prisma.user.findUnique({
     where: { user_id: userId },
   });
   return user;
-}
-
-export default {
-  findUserById
-}
+}*/
