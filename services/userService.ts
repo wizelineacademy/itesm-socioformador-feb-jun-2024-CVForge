6a006fe652a2558fedb7756143b667dc5ea6373b
gaelgarcia003 <105666768+AngelGaelGarciaRangel@users.noreply.github.com>
2024-04-17 10:44:33 -0600

    Added: User endpoints

--- services/userService.ts
+++ services/userService.ts
@@ -1,18 +1,35 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import prisma from '@/lib/prisma';
 import { Prisma } from '@prisma/client';
+import {User} from '@/types/user'
 
-
-const createUser = async () => {
-
-}
 const findUserById = async (userId: string) => {
-  const user = await prisma.user.findUnique({
-    where: { user_id: userId },
+  const user = await prisma.users.findUnique({
+    where: { users_id: userId },
   });
   return user;
+};
+
+const findUsers = async() => {
+  const users = await prisma.users.findMany();
+  return users;
 }
 
+const createUser = async (userData: User) => {
+  const user = await prisma.users.create({
+     data: {
+       ...userData,
+       last_login: new Date(),
+       created_at: new Date(),
+       updated_at: new Date(),
+     },
+  });
+  return user;
+ };
+
+
 export default {
-  findUserById
+  findUserById, 
+  createUser,
+  findUsers
 }
