c1855e4522651c11cefcfddbe99eb50aab6b27ad
gaelgarcia003 <105666768+AngelGaelGarciaRangel@users.noreply.github.com>
2024-04-17 11:47:20 -0600

    Added: Types for Professional Information

---
+++ types/experience.ts
@@ -0,0 +1,12 @@
+import { ProfessionalInfo } from "./professional";
+
+export type WorkExperience = {
+  work_experience_id: string;
+  work_position?: string | null;
+  description?: string | null;
+  start_date?: Date | null;
+  end_date?: Date | null;
+  professional_info_id?: string | null;
+  professional_info?: ProfessionalInfo | null;
+ };
+ 
