c752deb03bceae37138f865110bc768dbe964a20
Peco1503 <102172862+Peco1503@users.noreply.github.com>
2024-04-18 12:01:58 -0600

    modified:services and types

--- types/recommendation.ts
+++ types/recommendation.ts
@@ -5,5 +5,5 @@ export type Recommendation = {
     cv_insight_id?: string | null;
     title?: string | null;
     main_content?: string | null;
-    cv_insight?: CvInsight | null;
+    //cv_insight?: CvInsight | null;
 }
