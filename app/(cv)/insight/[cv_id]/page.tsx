813661a69a9a8dd68e4f80de082b1bac2fb3a4ce
Marco Podesta <59153650+marcopod@users.noreply.github.com>
2024-04-19 12:34:03 -0600

    pdf reader added

--- app/(cv)/insight/[cv_id]/page.tsx
+++ app/(cv)/insight/[cv_id]/page.tsx
@@ -1,12 +1,32 @@
 "use client";
-import React from "react";
 
-const Insight: React.FC = ({ }) => {
-    return (
-        <>
-        Insight
-        </>
-    )
+import { useState } from 'react';
+import { pdfjs, Document, Page } from 'react-pdf';
+
+if (typeof window !== 'undefined') {
+    pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+        'pdfjs-dist/build/pdf.worker.min.js',
+        import.meta.url
+    ).toString();
 }
 
-export default Insight;
+const MyApp = () => {
+    const [numPages, setNumPages] = useState<number>();
+    const [pageNumber, setPageNumber] = useState<number>(1);
+    const file = '/pdf/q_learning_notes.pdf';
+
+    function onDocumentLoadSuccess({ numPages }: { numPages: number }): void {
+        setNumPages(numPages);
+    }
+
+    return (
+        <div>
+            <Document file={file} onLoadSuccess={onDocumentLoadSuccess}>
+                <Page pageNumber={pageNumber} renderTextLayer={false} />
+            </Document>
+            <p>Page {pageNumber} of {numPages}</p>
+        </div>
+    );
+}
+
+export default MyApp;
---
+++ next.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  webpack: (config) => {
+    config.resolve.alias.canvas = false;
+    return config;
+  },
+};
