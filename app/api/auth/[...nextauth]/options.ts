b821b0393ac7ab4ee71438681c9e90858916fda0
a01721732 <68079249+a01721732@users.noreply.github.com>
2024-04-22 11:02:13 -0600

    Error Fixes

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -1,3 +1,4 @@
+/*
 import { prisma } from "@/lib/prisma";
 import { session } from "@/lib/session"
 import type { NextAuthOptions } from "next-auth";
@@ -23,7 +24,7 @@ export const options: NextAuthOptions = {
                 throw new Error('No profile')
             }
 
-            await prisma.user.upsert({
+            await prisma.users.upsert({
                 where: {
                     email: profile.email,
                 },
@@ -41,7 +42,7 @@ export const options: NextAuthOptions = {
         session,
         async jwt({ token, user, account, profile }) {
             if (profile) {
-                const user = await prisma.user.findUnique({
+                const user = await prisma.users.findUnique({
                     where: {
                         email: profile.email,
                     },
@@ -49,9 +50,124 @@ export const options: NextAuthOptions = {
                 if (!user) {
                     throw new Error('No user found')
                 }
-                token.id = user.user_id;
+                token.id = user.users_id;
             }
             return token
         },
     },
 }
+*/
+
+/*
+import { NextAuthOptions, User, getServerSession } from "next-auth";
+import { useSession } from "next-auth/react";
+import { redirect, useRouter } from "next/navigation";
+
+import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
+import GithubProvider from "next-auth/providers/github";
+
+import prisma from "@/lib/prisma";
+import { users } from "@prisma/client";
+
+export const options: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Email",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "example@example.com",
+        },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials || !credentials.email || !credentials.password)
+          return null;
+
+        const dbUser = await prisma.users.findFirst({
+          where: { email: credentials.email },
+        });
+
+        //Verify Password here
+        //We are going to use a simple === operator
+        //In production DB, passwords should be encrypted using something like bcrypt...
+        if (dbUser && dbUser.password === credentials.password) {
+          const { password, created_at, users_id, ...dbUserWithoutPassword } = dbUser;
+          return dbUserWithoutPassword as users;
+        }
+
+        return null;
+      },
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    }),
+  ],
+};
+
+export async function loginIsRequiredServer() {
+  const session = await getServerSession(options);
+  if (!session) return redirect("/");
+}
+
+export function loginIsRequiredClient() {
+  if (typeof window !== "undefined") {
+    const session = useSession();
+    const router = useRouter();
+    if (!session) router.push("/");
+  }
+}
+*/
+
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import db from '@/lib/prisma'
+import bcrypt from 'bcrypt'
+import { AuthOptions } from 'next-auth'
+
+export const options: AuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password", placeholder: "*****" },
+      },
+      async authorize(credentials, req) {
+        if (!credentials?.email) return null
+        console.log(credentials)
+
+        const userFound = await db.users.findFirst({
+            where: {
+                email: credentials?.email,
+                // users_id: ""
+            },
+        })
+
+        if (!userFound) throw new Error('No user found')
+
+        console.log(userFound)
+
+        const matchPassword = await bcrypt.compare(credentials.password, userFound.password)
+
+        if (!matchPassword) throw new Error('Wrong password')
+
+        return {
+            id: userFound.users_id,
+            email: userFound.email,
+        }
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/login",
+  },
+  
+};
--- services/user.ts
+++ services/user.ts
@@ -4,8 +4,8 @@ import prisma from '@/lib/prisma';
 const baseUrl = process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_API_URL : 'http://localhost:3001';
 
 export async function findUserById(userId: string) {
-  const user = await prisma.user.findUnique({
-    where: { user_id: userId },
+  const user = await prisma.users.findUnique({
+    where: { users_id: userId },
   });
   return user;
 }
