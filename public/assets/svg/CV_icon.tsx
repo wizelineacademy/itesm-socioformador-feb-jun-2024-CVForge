cbc462aac07ece76e41f6ae4eeecd5ba54462290
Marco Podesta <59153650+marcopod@users.noreply.github.com>
2024-04-23 10:29:35 -0600

    merge fixes

--- public/assets/svg/CV_icon.tsx
+++ public/assets/svg/CV_icon.tsx
@@ -6,7 +6,7 @@ interface LiveIconProps extends SVGProps<SVGSVGElement> {
 }
 
 const SvgComponent = ({ strokeColor = "#7E7E7E", ...props }: LiveIconProps) => (
-    <svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 34 34" viewBox="0 0 34 34" id="Cv">
+    <svg xmlns="http://www.w3.org/2000/svg" enableBackground="new 0 0 34 34" viewBox="0 0 34 34" id="Cv">
         <path d="M30,4.8C30,2.7,28.3,1,26.2,1H7.8C5.7,1,4,2.7,4,4.8v24.4C4,31.3,5.7,33,7.8,33h18.4c2.1,0,3.8-1.7,3.8-3.8V4.8z M28,29.2
 		c0,1-0.8,1.8-1.8,1.8H7.8c-1,0-1.8-0.8-1.8-1.8V4.8C6,3.8,6.8,3,7.8,3h18.4c1,0,1.8,0.8,1.8,1.8V29.2z" fill={strokeColor} 
         className="color000000 svgShape"></path>
