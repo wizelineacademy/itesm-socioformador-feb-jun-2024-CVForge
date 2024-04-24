318359bf85fcf173f7abfc3450ce9ed8f77a25ac
gaelgarcia003 <105666768+AngelGaelGarciaRangel@users.noreply.github.com>
2024-04-24 11:04:01 -0600

    Added:CV delete feature

--- app/(cv)/cv_gallery/components/ExistingCV.tsx
+++ app/(cv)/cv_gallery/components/ExistingCV.tsx
@@ -1,18 +1,27 @@
-import React from 'react';
-import {cv} from "@prisma/client"
+"use-client";
+import React from "react";
+import { cv } from "@prisma/client";
 
 interface ExistingCVProps {
-  title: string;
+  cvProp: cv;
+  handleCVSelection: (cvId: string) => void;
 }
 
-const ExistingCV: React.FC<{cvProp: cv}> = ({ cvProp }) => {
+const ExistingCV: React.FC<ExistingCVProps> = ({
+  cvProp,
+  handleCVSelection,
+}) => {
+  const handleClick = () => {
+    handleCVSelection(cvProp.cv_id);
+  };
   return (
-     <div className="flex flex-col items-center">
-       <div className="w-44 h-60 bg-white shadow-md flex items-center justify-center rounded-md">
-       </div>
-       <p className="mt-2 text-center text-primarygray">{cvProp.title}</p>
-     </div>
+    <div onClick={handleClick} className="flex flex-col items-center">
+      <div className="w-44 h-60 bg-white shadow-md flex items-center justify-center rounded-md">
+        {/* You can add content here */}
+      </div>
+      <p className="mt-2 text-center text-primarygray">{cvProp.title}</p>
+    </div>
   );
- };
- 
- export default ExistingCV;
+};
+
+export default ExistingCV;
