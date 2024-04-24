9ff95b6732523bc09bf949ac0790b077e2c9307d
Lebrijay02 <juan.lebrija2002@gmail.com>
2024-04-24 08:42:33 -0600

    pre-merge

--- app/(auth)/login/components/Login.tsx
+++ app/(auth)/login/components/Login.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import MainLogo from '@/public/assets/MainLogo'; 
 import Popup from './Popup';
+import SecondaryLogo from "@/public/assets/SecondaryLogo"
+
 
 const Login: React.FC= () => {
 
@@ -25,45 +27,53 @@ const Login: React.FC= () => {
     };
 
     return (
-        <div className="flex h-screen" style={{backgroundImage: 'url(/assets/bgimg.png)', backgroundSize: '100% 100%', backgroundRepeat: 'no-repeat'}}>
+        <div className='flex items-center justify-center h-screen'>
+            <div className='bg-white shadow-md flex items-between justify-center rounded-md p-10'>
+                <div className='flex flex-col items-center justify-center'>
+                    <div className="w-full h-10"><SecondaryLogo /></div>
+                    <p>Don’t Have an Account?</p>
+                </div>
+                <div className='flex flex-col items-center justify-center'>
+                    <h2>jassa</h2>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
+/*
+<div className="flex h-screen" >
             <div className="w-1/2 flex justify-center items-center">
-                {/* Left side - Logo */}
                 <div style={{ marginLeft: '40px' }}>
                     <MainLogo />
                 </div>
             </div>
-            <div className="w-1/2 flex justify-center items-center">
-                {/* Right side - Login form */}
+            <div className="w-1/2 flex justify-center items-center border border-aibliue">
                 <div className="text-center">
                     <h2 className="text-3xl text-inter font-bold mb-4 underline ">Try CVForge.ai now!</h2>
-                    {/* Signup buttons */}
                     <div className="mb-21">
                         <div className="mb-6">
                             <button className="text-gptgreen font-bold border-gradient rounded-lg" style={buttonStyle}>
                                 <img src="/assets/googleicon.png" className="mr-2" style={iconStyle} /> Sign up with Google
                             </button>
                         </div>
-                        <div className="mb-6"> {/* Add margin bottom for spacing */}
                             <button className="text-gptgreen font-bold border-gradient rounded-full" style={buttonStyle}>
                                 <img src="/assets/linkedinicon.png" className="mr-2" style={iconStyle} /> Sign up with LinkedIn
                             </button>
                         </div>
                     </div>
-                    {/* Line between Sign up with LinkedIn and Create Account */}
                     <div className = "flex"> 
                         <hr className="my-3 w-[80%] border-gray-400 flex"/>
                         <p className = "mx-2">or</p>
                         <hr className="my-3 w-[80%] border-gray-400 flex"/>
                     </div>
 
-                    {/* Create account button */}
                     <div className="mb-6 my-5">
                     <button onClick={togglePopup} className="bg-gradient-to-r from-emerald-400 to-cyan-400 text-white font-bold rounded-full mb-4" style={buttonStyle}>                            Create Account
                         </button>
                     </div>
-                    {/* Already have an account? */}
                     <p className="mb-2 my-20 text-inter font-bold">Already have an account?</p>
-                    {/* Sign in button */}
                     <button className="bg-gradient-to-r from-emerald-400 to-cyan-400 text-white font-bold rounded-full" style={buttonStyle}>
                         Sign In
                     </button>
@@ -71,7 +81,4 @@ const Login: React.FC= () => {
             </div>
             {isPopupVisible && <Popup onClose={togglePopup} />}
         </div>
-    );
-};
-
-export default Login;
+*/
