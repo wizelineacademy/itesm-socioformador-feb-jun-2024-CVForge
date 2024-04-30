cbcf1c39de90d3f77052f96761956e5f56068365
a01721732 <68079249+a01721732@users.noreply.github.com>
2024-04-30 10:43:50 -0600

    Implemented Credentials Provider

--- app/(landing)/components/SignInOutButton.tsx
+++ app/(landing)/components/SignInOutButton.tsx
@@ -8,7 +8,7 @@ const SignInOutButton = () => {
   if (session && session.user) {
     return (
       <div className="flex gap-4 ml-auto items-center">
-        <p className="text-sky-600">{session.user.name}</p>
+        <p className="text-sky-600">{session.user.email}</p>
         
         <button onClick={() => signOut()} className="hidden md:block bg-gradient-to-r from-emerald-400 to-cyan-400 text-white font-bold w-40 py-2 px-4 rounded">
           Sign Out
