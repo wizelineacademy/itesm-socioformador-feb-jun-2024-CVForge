a1788bc063d7636d29667eaac69146e65f7e184d
Marco Podesta <59153650+marcopod@users.noreply.github.com>
2024-05-01 17:34:28 -0600

    certificate tests

--- services/certificateService.ts
+++ services/certificateService.ts
@@ -1,5 +1,14 @@
 import prisma from '@/lib/prisma';
 import { Certificate } from '@/types/certificate';
+import { Prisma } from '@prisma/client';
+
+const createCertificate = async (certificateData: Prisma.certificateCreateInput) => {
+    const certificate = await prisma.certificate.create({
+        data: certificateData
+    });
+
+    return certificate;
+}
 
 const findCertificateById = async (certificateId: string) => {
     const certificate = await prisma.certificate.findUnique({
@@ -9,17 +18,22 @@ const findCertificateById = async (certificateId: string) => {
     return certificate;
 }
 
-const getAllCertificates = async() => {
+const findCertificateByProfessionalInfoId = async (professionalInfoId: string) => {
+    const certificates = await prisma.certificate.findMany({
+        where: { professional_info_id: professionalInfoId }
+        // where: {professional_info: {professional_info_id: professionalInfoId}}
+    })
+    return certificates
+}
+
+const getAllCertificates = async () => {
     const certificate = await prisma.certificate.findMany();
     return certificate;
 }
 
-const createCertificate = async (certificateData: Certificate) => {
-    const certificate = await prisma.certificate.create({
-        data: {
-            ...certificateData
-        }
-    });
-
-    return certificate;
+export default {
+    createCertificate,
+    findCertificateById,
+    findCertificateByProfessionalInfoId,
+    getAllCertificates
 }
