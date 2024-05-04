75b9fe01e8ed177fb66e6700b23581773005b551
Marco Podesta <59153650+marcopod@users.noreply.github.com>
2024-05-03 21:04:22 -0600

    merge with insight_ai

--- app/(cv)/insight/[cv_id]/CONSTANTS.ts
+++ app/(cv)/insight/[cv_id]/CONSTANTS.ts
@@ -42,35 +42,35 @@ export const RECOMMENDATIONS = [
 export const FETCHED_RECOMMENDATIONS = [
     {
         recommendation_id: "0",
-        cv_id: "123", 
+        cv_id: "123",
         title: "Get IBM Data Analytics certification",
         main_content: "Get an IBM Data Analytics certification to improve your credibility.",
         completed: true
     },
     {
         recommendation_id: "1",
-        cv_id: "123", 
+        cv_id: "123",
         title: "Learn React for Web Development",
         main_content: "Enhance your web development skills by learning React, a popular JavaScript library for building user interfaces.",
         completed: false
     },
     {
         recommendation_id: "2",
-        cv_id: "123", 
+        cv_id: "123",
         title: "Attend a Project Management Workshop",
         main_content: "Develop your project management skills by attending workshops that focus on methodologies like Agile and Scrum.",
         completed: false
     },
     {
         recommendation_id: "3",
-        cv_id: "123", 
+        cv_id: "123",
         title: "Read 'Thinking, Fast and Slow' by Daniel Kahneman",
         main_content: "Improve your decision-making and problem-solving skills by reading this insightful book on behavioral psychology.",
         completed: false
     },
     {
         recommendation_id: "4",
-        cv_id: "123", 
+        cv_id: "123",
         title: "Attend a Project Management Workshop",
         main_content: "Develop your project management skills by attending workshops that focus on methodologies like Agile and Scrum.",
         completed: false
@@ -112,20 +112,18 @@ export const RECOMMENDED_CHANGES = [
     "lorem ipsum",
 ]
 
-export const MOCK_CV = [
-    { 
-        achievement_name : "Award for Excellence in Leadership",
-        achievement_description: "Received award for outstanding leadership skills demonstrated in managing a team of 20+ members.",
-        work_experience_name: "XYZ Corporation",
-        work_experience_position: "Software Engineer",
-        work_experience_description: "Developed scalable web applications using modern technologies such as React.js and Node.js. Collaborated with cross-functional teams to deliver high-quality software solutions.",
-        work_experience_startdate: "January 2018",
-        work_experience_endate: "Present",
-        certificate_name: "AWS Certified Solutions Architect",
-        certificate_description: "Certification demonstrating proficiency in designing and deploying scalable, highly available, and fault-tolerant systems on Amazon Web Services (AWS).",
-        skills_title: "Programming Languages",
-        skills_level: "Advanced",
-        skills_duration: "5+ years",
-        skills_tag: "JavaScript, Python, Java, C++"
-    }
-]
+export const MOCK_CV = {
+    achievement_name: "Award for Excellence in Leadership",
+    achievement_description: "Received award for outstanding leadership skills demonstrated in managing a team of 20+ members.",
+    work_experience_name: "XYZ Corporation",
+    work_experience_position: "Software Engineer",
+    work_experience_description: "Developed scalable web applications using modern technologies such as React.js and Node.js. Collaborated with cross-functional teams to deliver high-quality software solutions.",
+    work_experience_startdate: "January 2018",
+    work_experience_endate: "Present",
+    certificate_name: "AWS Certified Solutions Architect",
+    certificate_description: "Certification demonstrating proficiency in designing and deploying scalable, highly available, and fault-tolerant systems on Amazon Web Services (AWS).",
+    skills_title: "Programming Languages",
+    skills_level: "Advanced",
+    skills_duration: "5+ years",
+    skills_tag: "JavaScript, Python, Java, C++"
+}
--- scripts/ai_test.py
+++ scripts/ai_test.py
@@ -2,6 +2,7 @@ import openai
 import os
 import json
 import sys
+import re
 
 api_key = os.environ.get("OPENAI_API_KEY")
 openai.api_key = api_key  # Set API key for the library
@@ -9,14 +10,17 @@ openai.api_key = api_key  # Set API key for the library
 def generate_recommendations(cv_data, job_position):
     try:
         # Craft prompt
+        
         prompt = f"Hello! I'm seeking advice on improving my CV for a {job_position} position. Below is the content of my CV:\n\n"
-        for section in cv_data:
-            for key, value in section.items():
-                prompt += f"CV section: {key}\nContent: {value}\n\n"
+        # for section in cv_data:
+        #     for key, value in section.items():
+        #         prompt += f"CV section: {key}\nContent: {value}\n\n"
+        for key, value in cv_data.items():
+            prompt += f"CV section: {key}\nContent: {value}\n\n"
         prompt += "\nBased on this information and the targeted position, could you please provide recommendations on how I can enhance my CV to better align with the requirements and stand out to potential employers?"
 
         # Generate recommendations
-        chat_completion = openai.ChatCompletion.create(
+        chat_completion = openai.chat.completions.create(
             model="gpt-3.5-turbo",
             messages=[
                 {
@@ -42,13 +46,24 @@ def parse_recommendations(recommendations):
 
 if __name__ == "__main__":
     try:
-        cv_data = json.loads(sys.argv[1])
+        # cv_data = json.loads(sys.argv[1])
+        data = sys.argv[1]
+
+        data_string_trimmed = data[1:-1]
+
+        # Splitting the string on commas not followed by space using regex
+        data_parts = re.split(r',(?![\s])', data_string_trimmed)
+
+        # Creating dictionary by splitting keys and values at colon
+        data_dict = {part.split(':')[0]: ':'.join(part.split(':')[1:]) for part in data_parts}
+
         job_position = sys.argv[2]
         
-        recommendations = generate_recommendations(cv_data, job_position)
+        recommendations = generate_recommendations(data_dict, job_position)
 
         # Print recommendations
         print("Recommendations to improve your CV:")
         print(recommendations)
     except Exception as e:
         print(f"An error occurred: {e}")
+
