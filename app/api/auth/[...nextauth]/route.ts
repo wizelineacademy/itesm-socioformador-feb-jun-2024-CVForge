d905258771bbca6d9554d18916e9a9d786a40c1e
a01721732 <68079249+a01721732@users.noreply.github.com>
2024-05-10 18:07:16 -0600

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,7 @@
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google";
+import LinkedInProvider from "next-auth/providers/linkedin"
 import CredentialsProvider from "next-auth/providers/credentials";
 import prisma from "@/lib/prisma";
 
@@ -16,6 +17,10 @@ const handler = NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID! as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET! as string,
+    }),
+    LinkedInProvider({
+      clientId: process.env.LINKEDIN_CLIENT_ID! as string,
+      clientSecret: process.env.LINKEDIN_CLIENT_SECRET! as string,
     }),
     CredentialsProvider({
       name: "Credentials",
