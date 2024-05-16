1e1ef36edd241e31ed88ca1f61e43be8963ec5fd
Lebrijay02 <juan.lebrija2002@gmail.com>
2024-05-16 10:35:34 -0600

    New CV Input

--- app/(cv)/cv_gallery/components/Gallery.tsx
+++ app/(cv)/cv_gallery/components/Gallery.tsx
@@ -6,6 +6,8 @@ import { getAllCVs, createCV, findCVById, deleteCV} from "@/services/cvService";
 import { useEffect, useState } from "react";
 import { getAllPositions } from "@/services/positionServices";
 import { cv, desired_position } from "@prisma/client";
+import { RxCross2 } from "react-icons/rx";
+
 
 const Gallery: React.FC = () => {
   //useState for CV
@@ -19,41 +21,41 @@ const Gallery: React.FC = () => {
   //useState for CV previsualization pop-up
   const [isDetailVisible, setIsDetailVisible] = useState(false);
   const [selectedCv, setSelectedCV] = useState<cv | null>(null);
-
+  
   //Fetching all CV information
   useEffect(() => {
     const fetchCvs = async () => {
       try {
         const cvArray = await getAllCVs();
-
+        
         setCvs(cvArray);
       } catch (error) {
         console.error("Failed to fetch CVs:", error);
       }
     };
-
+    
     fetchCvs();
   }, []);
-
+  
   //Fetching all Positions Information
   useEffect(() => {
     const fetchPositions = async () => {
       try {
         const positionsArray = await getAllPositions();
-
+        
         setPositions(positionsArray);
       } catch (error) {
         console.error("Failed to fetch Positions:", error);
       }
     };
-
+    
     fetchPositions();
   }, []);
-
+  
   const handleFormToggle = () => {
     setIsFormVisible(!isFormVisible);
   };
-
+  
   const handleFormSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
     try {
@@ -62,12 +64,12 @@ const Gallery: React.FC = () => {
           title: title,
           desired_position_id: selectedPosition,
         });
-
+        
         setCvs((cvs) => [...cvs, newCv]);
         setIsFormVisible(false);
         setTitle("");
         setSelectedPosition("");
-
+        
         // Handle successful form submission (e.g., close form, reset form)
       } else {
         console.error("No position selected. Please choose a position.");
@@ -83,93 +85,93 @@ const Gallery: React.FC = () => {
     setCvs((prevCvs) => prevCvs.filter(cv => cv.cv_id !== cvId));
     console.log("cv deleted");
   };
-
+  
   const handlePositionChange = (
     event: React.ChangeEvent<HTMLSelectElement>
   ) => {
     setSelectedPosition(event.target.value);
   };
-
+  
   const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setTitle(event.target.value);
   };
-
+  
   return (
     <div className="min-h-screen bg-transparent">
       <div className="grid xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-3 gap-2 overflow-y-auto top-0">
         <NewCv handleFormToggle={handleFormToggle} />
         {cvs.map((cv, index) => (
           <ExistingCV
-            key={index}
-            cvProp={cv}
-            deleteFunction={handleCVDelete}
+          key={index}
+          cvProp={cv}
+          deleteFunction={handleCVDelete}
           />
         ))}
       </div>
-
+    
       {/*pop up to create new*/}
       {isFormVisible && (
         <div className="fixed top-0 left-0 w-full h-full bg-opacity-50 bg-black flex justify-center items-center">
-          <div className="bg-white p-6 rounded-md shadow-md">
-            <h2>Create New CV</h2>
-            {/* Add your form component here */}
-            {/* Replace the example form with your actual form */}
-            <form onSubmit={handleFormSubmit}>
-              {/* Example form input */}
-              <input
-                type="text"
-                placeholder="New CV Title"
-                value={title}
-                onChange={handleTitleChange}
-                required
-                // Add more form fields as needed
-              />
-              <div className="form-group">
-                <label htmlFor="position">Select a position:</label>
-                <select
-                  id="position"
-                  value={selectedPosition}
-                  onChange={handlePositionChange}
-                  required
-                >
-                  <option value="" disabled>
-                    Choose a position
-                  </option>
-                  {positions.map((position, index) => (
-                    <option key={index} value={position.desired_position_id}>
-                      {position.title}
-                    </option>
-                  ))}
-                </select>
-              </div>
-              {/* Example submit button */}
-              <button type="submit">Create</button>
-              {/* Button to close the form */}
-              <button type="button" onClick={handleFormToggle}>
-                Cancel
-              </button>
-            </form>
+          {/* Pop Up White Box */}
+          <div className="bg-white px-2 rounded-md shadow-md flex flex-col">
+            {/* Top */}
+            <div className="flex flex-row p-2 justify-center items-center">
+            <button type="button" onClick={handleFormToggle} className="text-2xl text-secondarygray hover:text-opacity-100 text-opacity-50">
+              <RxCross2 />
+            </button>
+            <h1 className="text-primarygray font-koh_santepheap font-bold text-3xl mx-auto">New CV</h1>
           </div>
-        </div>
-      )}
+          {/* Middle */}
+          <form onSubmit={handleFormSubmit}>
+            <div className="px-6">
+              <div className="h-0.5 rounded bg-secondarygray w-auto bg-opacity-30"/>
+              <div className="p-4">
+                <div className="px-5 py-2">
+                  <div className='flex flex-col justify-left'>
+                    <p className='text-primarygray font-semibold font-inter text-md pb-0.5'>CV Title</p>
+                    <input
+                      type="text"
+                      placeholder="New CV Title"
+                      value={title}
+                      className="border-2 border-gptgreen bg-white h-10 px-3 pr-60 rounded-lg text-md focus:outline-none"
+                      onChange={handleTitleChange}
+                      required
+                    />
+                  </div>
+                  <div className="p-3"/>
+                  <p className='text-primarygray font-semibold font-inter text-md pb-0.5'>Position to Apply</p>
+                  <div className="form-group">
+                    <select
+                      id="position"
+                      value={selectedPosition}
+                      onChange={handlePositionChange}
+                      className="border-2 border-secondarygray border-opacity-50 text-secondarygray bg-white h-10 px-3 pr-32 rounded-lg text-md focus:outline-none mb-1"
+                      required
+                    >
+                      <option value="" disabled>
+                        Select a position
+                      </option>
+                      {positions.map((position, index) => (
+                        <option key={index} value={position.desired_position_id}>
+                        {position.title}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
+                </div>
+              </div>
+              <div className="h-0.5 rounded bg-secondarygray w-auto bg-opacity-30"/>
+            </div>
+            {/* Bottom */}
+            <button type="submit" className='flex items-center justify-center bg-gradient-to-r from-aiblue to-gptgreen felx-row text-white text-md rounded-3xl p-2.5 w-60 shadow-sm mx-auto my-4 delay-50 hover:scale-105 duration-200'>
+              Create
+            </button>
+          </form>
+      </div>
+      </div>
+    )}
     </div>
   );
 };
 
 export default Gallery;
-/*
-  //Deleting a CV
-  const handleCVDelete = async(cvId: string) => {
-    const deletedCV = await deleteCV(cvId);
-    setIsDetailVisible(false);
-    setCvs((prevCvs) => prevCvs.filter(cv => cv.cv_id !== cvId));
-    console.log("cv deleted");
-  };
-
-  //Fecthing CV individual information
-  const handleCVSelection = async (cvId: string) => {
-    const cvSelected = await findCVById(cvId);
-    setSelectedCV(cvSelected);
-    setIsDetailVisible(true);
-  };
-*/
--- app/(cv)/cv_gallery/components/NewCv.tsx
+++ app/(cv)/cv_gallery/components/NewCv.tsx
@@ -6,12 +6,12 @@ interface NewCvProps {
 
 const NewCv: React.FC<NewCvProps> = ({ handleFormToggle }) => {
   return (
-    <div className="flex flex-col items-center mx-5">
+    <div className="flex flex-col items-center mx-5 ">
       {/* Button with onClick event to toggle the form */}
       <button className="w-44 h-60 bg-white shadow-md flex items-center justify-center rounded-md"
         onClick={handleFormToggle}
       >
-        <span className="text-4xl text-outlinegray">+</span>
+        <span className="text-4xl text-outlinegray delay-50">+</span>
       </button>
       <p className="mt-2 text-center text-primarygray">New CV</p>
     </div>
