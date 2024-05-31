21df32fad88ad108d3016ef42b01823b3992f4de
Lebrijay02 <juan.lebrija2002@gmail.com>
2024-05-31 11:59:22 -0600

    jj

---
+++ cypress/e2e/cv/accessroadmap.cy.ts
@@ -0,0 +1,14 @@
+describe('template spec', () => {
+    it('passes', () => {
+      cy.visit('http://localhost:3000/')
+      cy.contains('AC').click()
+      cy.contains('9').click()
+      cy.contains('+/-').click()
+      cy.get('.component-display').first().should('contain.text', '-')
+      cy.contains('x').click()
+      cy.contains('7').click()
+      cy.contains('=').click()
+      cy.get('.component-display').first().should('contain.text', '-')
+      //cy.get('.component-display').first().should('not.have.text', '10')
+    })
+  })
