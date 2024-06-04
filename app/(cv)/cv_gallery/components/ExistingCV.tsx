e7d07afdab10ba32bf6aeb26ee114a02623a25c8
Lebrijay02 <juan.lebrija2002@gmail.com>
2024-06-04 14:01:13 -0600

    hell yeah

--- app/(cv)/cv_gallery/components/ExistingCV.tsx
+++ app/(cv)/cv_gallery/components/ExistingCV.tsx
@@ -1,23 +1,49 @@
 "use-client";
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { cv } from "@prisma/client";
 import Dropdown from './Dropdown';
+import Image from 'next/image'; // Import the Image component from next/image
 
 interface ExistingCVProps {
   cvProp: cv;
   deleteFunction: ((cvId: string) => void);
 }
 
+const cvData = [
+  { id: 'CV_Juan', imageUrl: '/assets/moccvs/CV-Juan.jpg' },
+  { id: 'CV_Franco', imageUrl: '/assets/moccvs/CV-Franco.jpg' },
+  { id: 'CV_Yuvan', imageUrl: '/assets/moccvs/CV-Yuvan.jpg' },
+  { id: 'CV_Gael', imageUrl: '/assets/moccvs/CV-Gael.jpg' },
+  { id: 'CV_Jerry', imageUrl: '/assets/moccvs/CV-Jerry.jpg' },
+];
+
 const ExistingCV: React.FC<ExistingCVProps> = ({ cvProp, deleteFunction }) => {
   const [isHovered, setIsHovered] = useState(false);
- 
+  const [selectedCv, setSelectedCv] = useState(null);
+
+  const selectRandomCv = () => {
+    const randomIndex = Math.floor(Math.random() * cvData.length);
+    setSelectedCv(cvData[randomIndex]);
+  };
+
+  useEffect(() => {
+    selectRandomCv();
+  }, []);
+
   return (
      <div className="flex flex-col items-center mx-5">
        <a href={`cv/${cvProp.cv_id}`}
-         className="w-44 h-60 bg-white shadow-md flex items-center justify-center rounded-md relative cursor-pointer"
+         className="relative cursor-pointer"
          onMouseEnter={() => setIsHovered(true)}
          onMouseLeave={() => setIsHovered(false)}
        >
+         {selectedCv? (
+           <div className="w-44 h-60 relative">
+             <Image src={selectedCv.imageUrl} alt={selectedCv.id} layout="fill" objectFit="cover" className="rounded-md shadow-md" />
+           </div>
+         ) : (
+           <div className="w-44 h-60 bg-white shadow-md flex items-center justify-center rounded-md relative cursor-pointer"></div>
+         )}
          {isHovered && (
            <div className="absolute top-0 right-0 p-3 z-10" onClick={(e) => e.preventDefault()}>
              <Dropdown cvProp={cvProp} deleteFunction={deleteFunction} />
@@ -25,10 +51,10 @@ const ExistingCV: React.FC<ExistingCVProps> = ({ cvProp, deleteFunction }) => {
          )}
        </a>
        <div className="flex flex-row p-2">
-         <p className="w-36 text-center text-primarygray truncate ...">{cvProp.title}</p>
+         <p className="w-36 text-center text-primarygray truncate...">{cvProp.title}</p>
        </div>
      </div>
   );
- };
+};
 
 export default ExistingCV;
--- app/(landing)/call_to_actions/Landing_Start.tsx
+++ app/(landing)/call_to_actions/Landing_Start.tsx
@@ -21,7 +21,7 @@ const { data: session } = useSession();
                 <div className='w-auto h-0.5 bg-primarygray rounded-xl my-3'/>
                 {/* Content */}
                 <p className='text-secondarygray text-md font-inter'>
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+                    Welcome to CVForge, a pioneering AI-powered platform transforming CV creation. We help job seekers, starting with computer science students, craft tailored, position-specific resumes and provide personalized guidance to enhance their employment prospects and streamline communication with employers.                
                 </p>
             </div>
             {/* Button */}
