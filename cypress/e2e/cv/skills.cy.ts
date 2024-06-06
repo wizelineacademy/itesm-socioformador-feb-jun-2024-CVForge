b0f915e31855bcc4168a1beb8e59fd88e9c58865
Peco1503 <102172862+Peco1503@users.noreply.github.com>
2024-06-06 09:13:15 -0600

    Added: All e2e Editor tests

---
+++ cypress/e2e/cv/skills.cy.ts
@@ -0,0 +1,33 @@
+describe('Work Experience Editor Form Test', () => {
+    beforeEach(() => {
+        cy.visit('http://localhost:3000/login');
+        cy.get('input[type=email]').type('francoelugo@live.com');
+        cy.get('input[type=password]').type('123');
+        cy.get('form').submit();
+
+        cy.url().should(url => {
+            expect(url).to.match(/\/cv_gallery|\/editor/);
+          });
+  
+          // navegar al editor
+          cy.url().then(url => {
+            if (!url.includes('/editor')) {
+              cy.visit('http://localhost:3000/editor');
+            }
+        });
+    });
+
+    it ('Updates skills section', () => {
+        cy.get('ul.steps').find('button').contains('Skills').click();
+        cy.get('button').contains('Add Skill').click();
+
+        // Fill out the form
+        cy.get('input[name="title"]').type('AWS');
+        cy.get('select[name="proficiency"]').select('Basic');
+
+        // Save the new work experience 
+        cy.get('button').contains('Save').click();
+
+    });
+
+});
