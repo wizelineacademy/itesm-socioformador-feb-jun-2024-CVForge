7bf252c443fadb5f16fb17433e93c7244599cfc3
gaelgarcia003 <105666768+AngelGaelGarciaRangel@users.noreply.github.com>
2024-06-13 18:40:43 -0600

    Refactorization on services and types

---
+++ services/achievementService.ts
@@ -0,0 +1,41 @@
+import prisma from '@/lib/prisma';
+import { Prisma } from '@prisma/client';
+
+const createAchievement = async (achievementData: Prisma.achievementCreateInput) => {
+    const achievement = await prisma.achievement.create({
+        data: {
+            ...achievementData
+        }
+    });
+
+    return achievement;
+}
+
+const findAchievementById = async (achievementId: string) => {
+    const achievement = await prisma.achievement.findUnique({
+        where: { achievement_id: achievementId }
+    });
+
+    return achievement;
+}
+
+const findAchievementByProfessionalInfoId = async (professionalInfoId: string) => {
+    const achievement = await prisma.achievement.findMany({
+        where: { professional_info_id:  professionalInfoId}
+        // where: {professional_info: {professional_info_id: professionalInfoId}}
+    });
+
+    return achievement;
+}
+
+const getAllAchievements = async() => {
+    const achievement = await prisma.achievement.findMany();
+    return achievement;
+}
+
+export default {
+    createAchievement,
+    findAchievementById,
+    findAchievementByProfessionalInfoId,
+    getAllAchievements
+}
