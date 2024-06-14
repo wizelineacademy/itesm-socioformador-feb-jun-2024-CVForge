533a575c010e1c96ae9ebab136ca02a5e73ec6fb
YuvanThirukumaran1 <a00834121@tec.mx>
2024-06-13 21:02:21 -0600

    added cypress tests

--- cypress/e2e/cv/newcv.cy.ts
+++ cypress/e2e/cv/newcv.cy.ts
@@ -1,26 +1,23 @@
 describe('New CV Creation', () => {
-    beforeEach(() => {
-      // Assuming you have a route for the Gallery component
+  beforeEach(() => {
       cy.visit('http://localhost:3000/cv_gallery');
       cy.get('input[type="email"]').type('a00833951@tec.mx');
       cy.get('input[type=password]').type('123');
       cy.get('form').submit();
-    });
-  
-    it('should open the new CV form, fill out the fields, and submit the form', () => {
-      cy.contains('button', '+').click();
-  
-      cy.get('form').should('be.visible');
-        cy.get('input[placeholder="New CV Title"]').type('CV by Cypress');
-  
-      cy.get('select#position').select('Software Engineer');
-  
-      cy.get('button[type="submit"]').click();
-  
-      cy.contains('div', 'My New CV').should('be.visible');
-    });
 
+
+      cy.contains('New CV').should('be.visible'); 
   });
-  
-  export {};
-  
+
+  it('should open the new CV form, fill out the fields, and submit the form', () => {
+      cy.contains('button', '+').click(); 
+
+      cy.get('form').should('be.visible'); 
+      cy.get('input[placeholder="New CV Title"]').type('CV by Cypress'); 
+
+      cy.get('select#position').select('Software Engineer'); 
+
+      cy.get('button[type="submit"]').click();
+  });
+});
+export{};
--- cypress/e2e/cv/signoutfromhome.cy.ts
+++ cypress/e2e/cv/signoutfromhome.cy.ts
@@ -1,15 +1,18 @@
 describe('Sign out from home', () => {
     beforeEach(() => {
-        cy.visit('http://localhost:3000/cv_gallery');
+        cy.visit('http://localhost:3000/login');
         cy.get('input[type="email"]').type('juan.lebrija02@gmail.com');
         cy.get('input[type=password]').type('123');
         cy.get('form').submit();
+        cy.url().should('include', '/cv_gallery'); // Adjust '/cv_gallery' to match your expected URL
     });
   
     it('Once logged in, go to home and then sign out', () => {
-        cy.get('#to-home').click();
+        cy.visit('http://localhost:3000');
+        cy.url().should('eq', 'http://localhost:3000/'); // Adjust to match your home page URL
         cy.contains('Sign Out').click();
+
     });
-  });
-  export {};
-  
+});
+
+export{}
