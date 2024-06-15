705cf6844d967b65ca61b5c4b57525d86bc510dc
YuvanThirukumaran1 <a00834121@tec.mx>
2024-06-14 20:49:07 -0600

    Update education.cy.ts

--- cypress/e2e/cv/education.cy.ts
+++ cypress/e2e/cv/education.cy.ts
@@ -1,39 +1,58 @@
 describe('Education Editor Form Test', () => {
-    beforeEach(() => {
-        cy.visit('http://localhost:3000/login');
-        cy.get('input[type=email]').type('francoelugo@live.com');
-        cy.get('input[type=password]').type('123');
-        cy.contains('Sign In').click();
+  // Runs before each test case
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/login');
+    cy.get('input[type=email]').type('francoelugo@live.com');
+    cy.get('input[type=password]').type('123');
+    cy.get('form').submit();
+    cy.url().should((url) => {
+      expect(url).to.match(/\/cv_gallery|\/editor/);
+    });
 
-        cy.url().should(url => {
-            expect(url).to.match(/\/cv_gallery|\/editor/);
-          });
+    cy.url().then((url) => {
+      if (!url.includes('/editor')) {
+        cy.visit('http://localhost:3000/editor');
+      }
+    });
+  });
+
+  it('Updates the Education Section', () => {
+    cy.get('ul.steps').find('button').contains('Education').click();
+    cy.get('button').contains('Add New Education').click();
+
+    cy.get('input[name="school"]').type('UC Berkeley');
+    cy.get('input[name="education_degree"]').type('Bachelors in Computer Science');
+    cy.get('input[name="gpa"]').type('3.5');
+    cy.get('input[name="start_date"]').type('2018-01-04');
+    cy.get('input[name="end_date"]').type('2022-01-01');
+
+    cy.get('button').contains('Save').click();
+
+    cy.contains('UC Berkeley').should('be.visible');
+    cy.contains('Bachelors in Computer Science').should('be.visible');
+  });
+
+  it('Validates Education Form Fields', () => {
+    cy.get('ul.steps').find('button').contains('Education').click();
+    cy.get('button').contains('Add New Education').click();
+
+    cy.get('input[name="school"]').should('be.visible');
+    cy.get('input[name="education_degree"]').should('be.visible');
+    cy.get('input[name="gpa"]').should('be.visible');
+    cy.get('input[name="start_date"]').should('be.visible');
+    cy.get('input[name="end_date"]').should('be.visible');
+  });
+
+
+  it('Handles Invalid Form Submissions', () => {
+    cy.get('ul.steps').find('button').contains('Education').click();
+    cy.get('button').contains('Add New Education').click();
+
+    cy.get('button').contains('Save').click();
+
+  });
+
   
-          // navegar al editor
-          cy.url().then(url => {
-            if (!url.includes('/editor')) {
-              cy.visit('http://localhost:3000/editor');
-            }
-        });
-    });
+  });
 
-    it ('Renders education section correctly', () => {
-      cy.contains('Education').should('exist');
-    });
-
-    it ('Updated Education Section', () => {
-      cy.get('ul.steps').find('button').contains('Education').click();
-      cy.get('button').contains('Add New Education').click();
-
-      // Fill out the form
-      cy.get('input[name="school"]').type('UC Berkeley');
-      cy.get('input[name="education_degree"]').type('Bachelors in Computer Science');
-      cy.get('input[name="gpa"]').type('3.5');
-      cy.get('input[name="start_date"]').type('2018-01-01');
-      cy.get('input[name="end_date"]').type('2022-01-01');
-
-      cy.get('button').contains('Save').click();
-    });
-
-});
-
+export {};
